21d26dd9d54375b5fcf2426aba0f24fb7ba9bc98
CeC <cecatlett@gmail.com>
2017-12-22 15:42:10 -0600

    wrestling with oauth init

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'json'
 gem "figaro"
 gem "fitgem"
 gem "haml"
+gem 'fitbit-api-client', require: 'fitbit'
 
 ## Remove this if you don't need a twitter widget.
 gem 'twitter', '>= 5.9.0'
--- fitbit_authorizer.rb
+++ fitbit_authorizer.rb
@@ -3,9 +3,18 @@
 require "fitgem"
 require "yaml"
 
-config = Fitgem::Client.symbolize_keys YAML.load(File.open(".fitbit.yml"))
-client = Fitgem::Client.new config[:oauth]
-request_token = client.request_token
+#config = Fitgem::Client.symbolize_keys YAML.load(File.open(".fitbit.yml"))
+#client = Fitgem::Client.new config[:oauth]
+#request_token = client.request_token
+
+client = Fitbit::Client.new(
+  client_id: Settings.fitbit.client_id,
+  client_secret: Settings.fitbit.client_secret,
+  token: current_user.token,
+  refresh_token: current_user.refresh_token,
+  expires_at: current_user.expires_at)
+
+p client.activity
 
 puts "Go to http://www.fitbit.com/oauth/authorize?oauth_token=#{request_token.token} and authorize this widget to access your data. You'll receive a PIN code. Copy and paste that PIN below and hit <enter>:"
 verifier = gets.chomp
